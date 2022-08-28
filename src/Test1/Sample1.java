52dfdeadea737605c6e4ae76bb312559a3c28794
nc <nc@DESKTOP-H0G03DT>
2022-08-28 07:41:03 +0530

    [ANIS]

---
+++ src/Test1/Sample1.java
@@ -0,0 +1,14 @@
+package Test1;
+
+public class Sample1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("I Love My Country So Much");
+		
+		
+		System.out.println("I Love My Father and Mother");
+		
+	}
+
+}
